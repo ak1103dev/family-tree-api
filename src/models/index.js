446acfdf3a1a2a67b2c084325c48a7ca7c6d4825
ak1103dev <ak.apichan1103@gmail.com>
2018-12-09 23:14:05 +0700

    :construction: separate file

--- src/models/index.js
+++ src/models/index.js
@@ -2,11 +2,12 @@ const config = require('config')
 const mongoose = require('mongoose')
 
 const User = require('./user')
+const Profile = require('./profile')
 
 mongoose.Promise = global.Promise
 mongoose.connect(config.get('mongoUrl'))
 
 module.exports = {
   User,
-  AccessToken,
+  Profile,
 }
